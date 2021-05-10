319f892d34e382ea1b6d8958083baf3c5699ba03
olamide oladinni <olajide1987@gmail.com>
2021-05-10 01:37:14 -0500

    search box is displaying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
-function App() {
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMovieRequest = async () => {
+      const url = 'http://www.omdbapi.com/?s=iron man&apikey=f64b30cc';
+      const response = await fetch(url);
+      const responseJson = await response.json();
+
+      console.log(responseJson);
+      setMovies(responseJson.Search);
+  };
+
+  useEffect(() => {
+      getMovieRequest();
+      },
+ []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='container-fluid movie-app'>
+    <div className='row'>
+      <MovieListHeading heading='Movies' />
+      <SearchBox />
     </div>
+    <div className='row'>
+    <MovieList movies={movies} />
+  </div>
+</div>
   );
-}
+};
 
 export default App;
